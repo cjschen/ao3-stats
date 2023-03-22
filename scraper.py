d0910bfe83e5cd952dd2e90e8fc6b93e4d9ae8c6
CJ Chen <sijia1996@gmail.com>
2023-03-21 23:09:11 -0700

    Better Scraper

--- scraper.py
+++ scraper.py
@@ -9,36 +9,51 @@ from os import path, mkdir
 from datetime import datetime
 from constants import FANDOM, BASE_URL, START, END
 
-# https://archiveofourown.org/tags/Naruto/works?commit=Sort+and+Filter&page=2&utf8=%E2%9C%93&work_search%5Bcomplete%5D=&work_search%5Bcrossover%5D=&work_search%5Bdate_from%5D=&work_search%5Bdate_to%5D=&work_search%5Bexcluded_tag_names%5D=&work_search%5Blanguage_id%5D=&work_search%5Bother_tag_names%5D=&work_search%5Bquery%5D=&work_search%5Bsort_column%5D=kudos_count&work_search%5Bwords_from%5D=&work_search%5Bwords_to%5D=
-
 
 date = datetime.now().strftime("%Y%m%d")
-# FANDOM_page_datedownloaded
+last_date = datetime.max
+target_date = datetime.fromtimestamp(1679371365) # Mar 20
 
-if not path.exists(f"data/html/{FANDOM}"):
-    mkdir(f"data/html/{FANDOM}")
+if not path.exists(f"data/html/{FANDOM}/{date}"):
+    mkdir(f"data/html/{FANDOM}/{date}")
 
-for i in range(START, END + 1):
-    print(f"Downloading page {i} of {END}")
-    file_name= f"data/html/{FANDOM}/{FANDOM}_{i}_{date}.html"
+# target_date = datetime.fromtimestamp(1679198920) # Mar 19
+# target_date = datetime.fromtimestamp(1679285320) # Nov 2022
+index = 1
+
+while(last_date > target_date):
+        
+    file_name= f"data/html/{FANDOM}/{date}/{FANDOM}_{index}.html"
     # get initial
-    url = f"{BASE_URL}?page={i}"
-    resp = requests.get(BASE_URL)
+    url = f"{BASE_URL}?page={index}"
+    resp = requests.get(url)
 
     if not resp.ok:
         print(f"API error on request to {url} failed")
-        print(f"Failed at page: {i}")
+        print(f"Failed at page: {index}")
         print(resp.text)
         exit(-1)
-
-    
     soup = BeautifulSoup(resp.content, features="html.parser")
+    
     if not len(soup.select("ol .work")):
-        print(f"failed to find works on url {i}")
+        print(f"failed to find works on url {index}")
         exit(-1)
+    
+    
+    first_work_date = soup.select(".datetime")[0].string
+    first_date = datetime.strptime(first_work_date, "%d %b %Y")
+    if first_date < target_date:
+        print(f"First work published on {first_work_date}. Skipping download")
+        last_date = first_date
+        break
+
+    last_work_date = soup.select(".datetime")[-1].string
+    last_date = datetime.strptime(last_work_date, "%d %b %Y")
 
     with open(file_name, "w") as file:
         file.write(resp.text)
-    print(f"Finished downloading page {i} of {END}. Sleeping for 40s")
+    
+    print(f"Finished downloading page {index}. Last date: {last_work_date}. Sleeping for 40s")
 
     sleep(40)
+    index += 1
